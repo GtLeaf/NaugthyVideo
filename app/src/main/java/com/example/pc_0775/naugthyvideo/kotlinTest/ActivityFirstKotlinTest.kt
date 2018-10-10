3a66a20e0a89827c8070ca0c03fd71ad7f816554
[GtLeaf] <962755614@qq.com>
2018-10-10 10:31:44 +0800

    start first kotlin test

---
+++ app/src/main/java/com/example/pc_0775/naugthyvideo/kotlinTest/ActivityFirstKotlinTest.kt
@@ -0,0 +1,14 @@
+package com.example.pc_0775.naugthyvideo.kotlinTest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.example.pc_0775.naugthyvideo.R
+
+class ActivityFirstKotlinTest : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_kotlin_test)
+    }
+}
---
+++ app/src/main/java/com/example/pc_0775/naugthyvideo/kotlinTest/ActvityKotlinTest.kt
@@ -0,0 +1,5 @@
+package com.example.pc_0775.naugthyvideo.kotlinTest
+
+/**
+ * Created by PC-0775 on 2018/10/10.
+ */
