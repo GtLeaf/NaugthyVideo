9f84e0234da1aea83070c2dee2dcebf20a2acd3b
[GtLeaf] <962755614@qq.com>
2018-10-13 09:35:26 +0800

    import vitamio

---
+++ app/src/main/java/com/example/pc_0775/naugthyvideo/view/ActivityLiveBroadcastPlay.java
@@ -0,0 +1,48 @@
+package com.example.pc_0775.naugthyvideo.view;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.pc_0775.naugthyvideo.R;
+import com.example.pc_0775.naugthyvideo.base.BaseActivity;
+
+public class ActivityLiveBroadcastPlay extends BaseActivity {
+
+
+    @Override
+    public void initParams(Bundle params) {
+
+    }
+
+    @Override
+    public int bindLayout() {
+        return R.layout.activity_live_broadcast_play;
+    }
+
+    @Override
+    public View bindView() {
+        return null;
+    }
+
+    @Override
+    public void initView(View view) {
+
+    }
+
+    @Override
+    public void setListener() {
+
+    }
+
+    @Override
+    public void widgetClick(View v) throws Exception {
+
+    }
+
+    @Override
+    public void doBusiness(Context mContext) {
+
+    }
+}
