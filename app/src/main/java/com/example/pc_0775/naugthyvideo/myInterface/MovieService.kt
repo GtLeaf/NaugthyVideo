1c4d5d14cd78ea343df3c0a5b030d84f42bc7cfa
[GtLeaf] <962755614@qq.com>
2019-01-04 21:59:38 +0800

    add retrofit2 add getMovieItem() function

--- app/src/main/java/com/example/pc_0775/naugthyvideo/myInterface/MovieService.kt
+++ app/src/main/java/com/example/pc_0775/naugthyvideo/myInterface/MovieService.kt
@@ -10,6 +10,10 @@ import retrofit2.http.QueryMap
  * Created by PC-0775 on 2019/1/3.
  */
 interface MovieService {
+    //baseUrl = https://api.douban.com/v2/movie/
     @GET("in_theaters?")
     fun getMovie(@QueryMap params:Map<String, String>): Call<ResponseBody>
+
+    @GET("subject/{movieId}")
+    fun getMovieItem(@Path("movieId") movieId:Int ): Call<ResponseBody>
 }
