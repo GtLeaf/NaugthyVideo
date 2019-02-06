08d7b23a7eb1e17eeabb537fccfab45cd7ca871e
[GtLeaf] <962755614@qq.com>
2019-02-06 23:42:20 +0800

    add userinfoChange activity

---
+++ app/src/main/java/com/example/pc_0775/naugthyvideo/view/ActivityChangeUserInfo.kt
@@ -0,0 +1,27 @@
+package com.example.pc_0775.naugthyvideo.view
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.pc_0775.naugthyvideo.R
+import com.example.pc_0775.naugthyvideo.base.BaseActivityKotlin
+
+class ActivityChangeUserInfo : BaseActivityKotlin() {
+    override fun initParams(params: Bundle?) {
+    }
+
+    override fun bindLayout(): Int {
+        return R.layout.activity_change_user_info
+    }
+
+    override fun initView(view: View) {
+    }
+
+    override fun setListener() {
+    }
+
+    override fun doBusiness(mContext: Context) {
+    }
+
+}
