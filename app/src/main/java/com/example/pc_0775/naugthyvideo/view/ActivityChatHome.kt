24bf83fd3a7e730aa66c1c3f9b612982032b5b7e
[GtLeaf] <962755614@qq.com>
2019-02-07 23:33:11 +0800

    add chatRoomActivity

--- app/src/main/java/com/example/pc_0775/naugthyvideo/view/ActivityChatHome.kt
+++ app/src/main/java/com/example/pc_0775/naugthyvideo/view/ActivityChatHome.kt
@@ -6,21 +6,20 @@ import android.os.Bundle
 import android.view.View
 import com.example.pc_0775.naugthyvideo.R
 import com.example.pc_0775.naugthyvideo.base.BaseActivityKotlin
-import kotlinx.android.synthetic.main.activity_change_user_info.*
 
-class ActivityChangeUserInfo : BaseActivityKotlin() {
+class ActivityChatHome : BaseActivityKotlin() {
+
     override fun initParams(params: Bundle?) {
     }
 
     override fun bindLayout(): Int {
-        return R.layout.activity_change_user_info
+        return R.layout.activity_chat_home
     }
 
     override fun initView(view: View) {
     }
 
     override fun setListener() {
-        tb_change_user_info.setNavigationOnClickListener { v ->  finish() }
     }
 
     override fun doBusiness(mContext: Context) {
